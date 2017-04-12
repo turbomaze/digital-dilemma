53550131990b10508c7ab7799b1e97f6d655e23c
Anthony Liu <anthonyscottliu@gmail.com>
2017-04-12 03:11:28 -0400

    Toggles color of specific grid cells on an api hit.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,36 @@
 var DigitalDilemma = (function() {
   function initDigitalDilemma() {
+    var DELAY = 500;
+
     // socket stuff
-    socket.on('foo', function() {
-      console.log('bar');
+    function toggleCell(id) {
+      var oldColor = $(id).css('background-color');
+      $(id).css('background-color', 'rgb(200, 50, 50)');
+      setTimeout((function(colorToRestore) {
+        return function() {
+          $(id).css('background-color', colorToRestore);
+        };
+      })(oldColor), DELAY);
+    }
+    socket.on('toggle-1', function() {
+      console.log('swag');
+      toggleCell('#cell-5');
     });
+    socket.on('toggle-2', function() {
+      toggleCell('#cell-10');
+    });
+
+    // initialize grid colors
+    for (var i = 0; i < 16; i++) {
+      var id = '#cell-' + i;
+      var g = Math.floor(100 + 50 * Math.random());
+      var color = getColor(g, g, g);
+      $(id).css('background-color', color);
+    }
+  }
+
+  function getColor(r, g, b) {
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
   }
 
   return {
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,16 @@ var Game = require('../models/Game');
 var ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = function(io) {
+  // NORMAL API ENDPOINTS
+  router.get('/toggle/1', function(req, res, next) {
+    io.sockets.emit('toggle-1');
+    res.json({success: true});
+  });
+
+  router.get('/toggle/2', function(req, res, next) {
+    io.sockets.emit('toggle-2');
+    res.json({success: true});
+  });
+
   return router;
 };
