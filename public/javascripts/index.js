2bd4b204c1cf502caba93bb5f2dbc4ddcb82a0b6
Anthony Liu <anthonyscottliu@gmail.com>
2017-04-12 14:51:27 -0400

    Abstract the grid's state from the grid's view.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,14 @@
 var DigitalDilemma = (function() {
+  // CONFIG
   var WIDTH = 4;
   var HEIGHT = 4;
   var TOGGLE_DELAY = 500;
   var RANDOM_COLORER = false;
   var INTERVAL_DELAY = 100;
   var GRAY_MAG = 4;
+
+  // STATE VARIABLES
+  var grid = [];
 
   function initDigitalDilemma() {
     // socket stuff
@@ -19,14 +23,16 @@ var DigitalDilemma = (function() {
     // initialize the grid
     for (var i = 0; i < WIDTH; i++) {
       for (var j = 0; j < HEIGHT; j++) {
-        var index = j * WIDTH + i;
-        var div = document.createElement('div');
-        div.id = 'cell-' + index;
-        div.className = 'cell';
-        div.innerHTML = '&nbsp;';
-        document.getElementById('grid').appendChild(div);
+        // the state component of the grid
+        var cellId = j * WIDTH + i;
+        grid.push({
+          id: cellId
+        });
       }
     }
+
+    // load the grid in the html
+    populateGrid('grid', grid);
 
     // initialize grid colors
     RANDOM_COLORER = setInterval(
@@ -35,6 +41,17 @@ var DigitalDilemma = (function() {
     );
 
     // event listeners
+  }
+
+  // the UI component of the grid
+  function populateGrid(containerId, gridToLoad) {
+    for (var i = 0; i < gridToLoad.length; i++) {
+      var div = document.createElement('div');
+      div.id = 'cell-' + gridToLoad[i].id;
+      div.className = 'cell';
+      div.innerHTML = '&nbsp;';
+      document.getElementById(containerId).appendChild(div);
+    }
   }
 
   function randomlyColorGray() {
@@ -52,6 +69,17 @@ var DigitalDilemma = (function() {
     }
   }
 
+  function toggleCell(id) {
+    var oldColor = $(id).css('background-color');
+    $(id).css('background-color', 'rgba(200, 50, 50, 255)');
+    setTimeout((function(colorToRestore) {
+      return function() {
+        $(id).css('background-color', colorToRestore);
+      };
+    })(oldColor), TOGGLE_DELAY);
+  }
+
+  // HELPERS
   function extractColor(colorString) {
     if (colorString.indexOf('(') == -1) {
       return -1;
@@ -63,16 +91,6 @@ var DigitalDilemma = (function() {
       );
       return firstChannel;
     }
-  }
-
-  function toggleCell(id) {
-    var oldColor = $(id).css('background-color');
-    $(id).css('background-color', 'rgba(200, 50, 50, 255)');
-    setTimeout((function(colorToRestore) {
-      return function() {
-        $(id).css('background-color', colorToRestore);
-      };
-    })(oldColor), TOGGLE_DELAY);
   }
 
   function getColor(r, g, b) {
