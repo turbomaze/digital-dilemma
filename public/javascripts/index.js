b1cf0b128351603aaf73e2ef18f9582e347c4a3b
Anthony Liu <anthonyscottliu@gmail.com>
2017-04-12 14:44:51 -0400

    Implements the cool smooth gray shift effect.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,13 @@
 var DigitalDilemma = (function() {
-  function initDigitalDilemma() {
-    var DELAY = 500;
+  var WIDTH = 4;
+  var HEIGHT = 4;
+  var TOGGLE_DELAY = 500;
+  var RANDOM_COLORER = false;
+  var INTERVAL_DELAY = 100;
+  var GRAY_MAG = 4;
 
+  function initDigitalDilemma() {
     // socket stuff
-    function toggleCell(id) {
-      var oldColor = $(id).css('background-color');
-      $(id).css('background-color', 'rgb(200, 50, 50)');
-      setTimeout((function(colorToRestore) {
-        return function() {
-          $(id).css('background-color', colorToRestore);
-        };
-      })(oldColor), DELAY);
-    }
     socket.on('toggle-1', function() {
       console.log('swag');
       toggleCell('#cell-5');
@@ -20,17 +16,67 @@ var DigitalDilemma = (function() {
       toggleCell('#cell-10');
     });
 
+    // initialize the grid
+    for (var i = 0; i < WIDTH; i++) {
+      for (var j = 0; j < HEIGHT; j++) {
+        var index = j * WIDTH + i;
+        var div = document.createElement('div');
+        div.id = 'cell-' + index;
+        div.className = 'cell';
+        div.innerHTML = '&nbsp;';
+        document.getElementById('grid').appendChild(div);
+      }
+    }
+
     // initialize grid colors
-    for (var i = 0; i < 16; i++) {
+    RANDOM_COLORER = setInterval(
+      randomlyColorGray,
+      INTERVAL_DELAY
+    );
+
+    // event listeners
+  }
+
+  function randomlyColorGray() {
+    for (var i = 0; i < WIDTH * HEIGHT; i++) {
       var id = '#cell-' + i;
       var g = Math.floor(100 + 50 * Math.random());
-      var color = getColor(g, g, g);
-      $(id).css('background-color', color);
+      var oldGray = extractColor($(id).css('background-color'));
+      if (oldGray != -1 && oldGray < 150 && oldGray > 100) {
+        g = Math.floor(oldGray + (
+          -GRAY_MAG + 2 * GRAY_MAG * Math.random())
+        );
+        g = Math.max(100, Math.min(150, g));
+      }
+      $(id).css('background-color', getColor(g, g, g));
     }
   }
 
+  function extractColor(colorString) {
+    if (colorString.indexOf('(') == -1) {
+      return -1;
+    } else {
+      var firstParen = colorString.indexOf('(');
+      var firstComma = colorString.indexOf(',');
+      var firstChannel = parseInt(
+        colorString.substring(firstParen + 1, firstComma)
+      );
+      return firstChannel;
+    }
+  }
+
+  function toggleCell(id) {
+    var oldColor = $(id).css('background-color');
+    $(id).css('background-color', 'rgba(200, 50, 50, 255)');
+    setTimeout((function(colorToRestore) {
+      return function() {
+        $(id).css('background-color', colorToRestore);
+      };
+    })(oldColor), TOGGLE_DELAY);
+  }
+
   function getColor(r, g, b) {
-    return 'rgb(' + r + ',' + g + ',' + b + ')';
+    return 'rgba(' + r + ',' + g + ',' + b + ', 255)';
   }
 
   return {
